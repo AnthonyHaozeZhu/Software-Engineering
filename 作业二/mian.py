e8ff3fdfa6571541d9f9bccb91afdc80f70a31f3
AnthonyHaozeZhu <1119886232@qq.com>
2022-03-09 00:59:45 +0800

    初步完成作业一

---
+++ 作业二/mian.py
@@ -0,0 +1,24 @@
+from random import random
+from unittest import result
+import numpy as np 
+
+
+class MSA:
+    ls = []
+    result = 0
+    def __init__(self, ls):
+        self.ls = np.array(ls)
+    def Calc(self):
+        while self.ls.shape != (0,):
+            max_index = np.argmax(self.ls)
+            if self.ls[max_index] < 0:
+                break
+            else:
+                self.result += self.ls[max_index]
+                self.ls = np.delete(self.ls, max_index) 
+        return self.result
+
+
+
+a = MSA([-1, -1, 1, 2, 3])
+print(a.Calc())
