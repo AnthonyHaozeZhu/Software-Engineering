45f1ce08c2c16a278968dc4f173a95c637fc4ea0
AnthonyHaozeZhu <1119886232@qq.com>
2022-03-11 20:45:18 +0800

    使用Pycharm后更改格式

--- 作业二/mian.py
+++ 作业二/mian.py
@@ -1,13 +1,13 @@
-from random import random
-from unittest import result
-import numpy as np 
+import numpy as np
 
 
 class MSA:
     ls = []
     result = 0
+
     def __init__(self, ls):
         self.ls = np.array(ls)
+
     def Calc(self):
         while self.ls.shape != (0,):
             max_index = np.argmax(self.ls)
@@ -15,9 +15,8 @@ class MSA:
                 break
             else:
                 self.result += self.ls[max_index]
-                self.ls = np.delete(self.ls, max_index) 
+                self.ls = np.delete(self.ls, max_index)
         return self.result
-
 
 
 a = MSA([-1, -1, 1, 2, 3])
