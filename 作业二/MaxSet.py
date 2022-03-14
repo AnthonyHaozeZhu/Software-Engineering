a5f46f97f142e9a319c8456e7ba0d9f56f4e3724
AnthonyHaozeZhu <1119886232@qq.com>
2022-03-15 00:40:53 +0800

    完成基本功能，未进行性能测试

--- 作业二/MaxSet.py
+++ 作业二/MaxSet.py
@@ -7,9 +7,6 @@
 """
 
 
-import numpy as np
-
-
 class MSA:
     """
     用于计算集合中子集的最大值
@@ -21,57 +18,78 @@ class MSA:
         """
         self.array_ls = []
         self.result = 0
+        self.sub_list = []
 
-    def get_set(self):
+    def get_set(self, in_set):
         """
         给集合赋值
         :return:
         """
-        set_length = 0
-        try:
-            set_length = int(input('请输入集合长度:'))
-        except ValueError:
-            print("Error: 请输入整数")
-
-        if set_length < 0:
-            raise Exception("请输入大于0的整数")
-
-        while set_length > 0:
-            try:
-                self.array_ls.append(int(input("请输入该集合的元素")))
-                set_length -= 1
-            except ValueError:
-                print("Error: 请输入整数")
-        self.array_ls = np.array(self.array_ls)
+        self.array_ls = in_set
 
     def sum_maxset(self):
         """
         计算最大值集合
         :return: 最大值
         """
-        while self.array_ls.shape != (0,):
-            max_index = np.argmax(self.array_ls)
-            if self.array_ls[max_index] < 0:
+        # 判断是否全为负数
+        max_item = float('-inf')
+        is_allneg = True
+        for index in self.array_ls:
+            if index >= 0:
+                is_allneg = False
                 break
-            self.result += self.array_ls[max_index]
-            self.array_ls = np.delete(self.array_ls, max_index)
+            if index > max_item:
+                max_item = index
+        if is_allneg:
+            return max_item
+        # 如果不是
+        total = 0
+        total_length = len(self.array_ls)
+        for i in range(total_length):
+            total += self.array_ls[i]
+            if total > self.result:
+                self.result = total
+                self.sub_list.append(i)
+            elif total < 0:
+                total = 0
+                self.sub_list.clear()
         return self.result
 
+    def get_sublist(self):
+        """
+        获得最大数组
+        :return: 最大值集合
+        """
+        return self.sub_list
 
-# def main():
-#     """
-#     初始化该类
-#     :return:
-#     """
-#     msa = MSA()
-#     msa.get_set()
-#     msa.sum_maxset()
-#
-#
-# if __name__ == '__main__':
-#     print("hello world!")
-#     main()
 
-msa = MSA()
-msa.get_set()
-msa.sum_maxset()
+def main(the_set):
+    """
+    自动进行生成
+    :param msa: 初始化过的MSA类
+    :return:求得的值
+    """
+    msa = MSA()
+    msa.get_set(in_set=the_set)
+    return msa.sum_maxset()
+
+
+if __name__ == '__main__':
+    array_ls = []
+    try:
+        length = int(input('请输入集合长度:'))
+    except ValueError:
+        print("Error: 请输入整数")
+
+    if length < 0:
+        raise Exception("请输入大于0的整数")
+
+    while length > 0:
+        try:
+            array_ls.append(int(input("请输入该集合的元素")))
+            length -= 1
+        except ValueError:
+            print("Error: 请输入整数")
+    main(array_ls)
+    print("最终结果是", main(array_ls))
--- 作业二/try_unittest.py
+++ 作业二/try_unittest.py
@@ -8,16 +8,18 @@
 
 
 import unittest
-from MaxSet import MSA
+import MaxSet
 
 
 class Testing(unittest.TestCase):
     """
-
+    测试
     """
 
     def test_main(self):
-        self.assertEqual(main(), 0)
-
+        self.assertEqual(MaxSet.main([-1, 2, 3, -4]), 5)
+        self.assertEqual(MaxSet.main([-1, 2, -5, 3, -4]), 3)
+        self.assertEqual(MaxSet.main([-1, 20, -5, 30, -4]), 45)
+        self.assertEqual(MaxSet.main([-2, -3, -5, -1, -9]), -1)
 
 
